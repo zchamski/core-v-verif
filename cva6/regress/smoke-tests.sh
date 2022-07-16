fb76de80bdca055fb51861c830b560107dd02d9a
Zbigniew Chamski <zbigniew.chamski@thalesgroup.com>
2022-07-16 21:30:55 +0200

    Fix incorrect name of simulator configuration.
    * cva6/regress/smoke-tests.sh (DV_SIMULATORS): Correct name of Verilator
      test harness configuration.
    
    Signed-off-by: Zbigniew Chamski <zbigniew.chamski@thalesgroup.com>

--- cva6/regress/smoke-tests.sh
+++ cva6/regress/smoke-tests.sh
@@ -20,7 +20,7 @@ source ./cva6/regress/install-riscv-compliance.sh
 source ./cva6/regress/install-riscv-tests.sh
 
 if ! [ -n "$DV_SIMULATORS" ]; then
-  DV_SIMULATORS=verisim-testharness,spike
+  DV_SIMULATORS=veri-testharness,spike
 fi
 
 cd cva6/sim/
