81688cb0f132362a3192daff86083675b57b3a6d
Zbigniew Chamski <zbigniew.chamski@thalesgroup.com>
2022-07-16 17:00:16 +0200

    [WIP] Adapt test CFLAGS to the GCC12/CVA6 combo: use RISC-V ISA spec v2.2.
    * cva6/regress/smoke-tests.sh (hello_world): Force use of RISC-V ISA spec
      v2.2.
    * cva6/tests/testlist_riscv-compliance-cv32a60x.yaml (all tests): Ditto.
    * cva6/tests/testlist_riscv-tests-cv32a60x-p.yaml (all tests): Ditto.
    
    Signed-off-by: Zbigniew Chamski <zbigniew.chamski@thalesgroup.com>

--- cva6/regress/smoke-tests.sh
+++ cva6/regress/smoke-tests.sh
@@ -29,7 +29,7 @@ python3 cva6.py --testlist=../tests/testlist_riscv-tests-cv64a6_imafdc_sv39-p.ya
 python3 cva6.py --testlist=../tests/testlist_riscv-compliance-cv64a6_imafdc_sv39.yaml --test rv32i-I-ADD-01 --iss_yaml cva6.yaml --target cv64a6_imafdc_sv39 --iss=$DV_SIMULATORS $DV_OPTS
 python3 cva6.py --testlist=../tests/testlist_custom.yaml --test custom_test_template --iss_yaml cva6.yaml --target cv64a6_imafdc_sv39 --iss=$DV_SIMULATORS $DV_OPTS
 python3 cva6.py --target cv64a6_imafdc_sv39 --iss=$DV_SIMULATORS --iss_yaml=cva6.yaml --c_tests ../tests/custom/hello_world/hello_world.c\
-  --gcc_opts "-g ../tests/custom/common/syscalls.c ../tests/custom/common/crt.S -I../tests/custom/env -I../tests/custom/common -T ../tests/custom/common/test.ld"
+  --gcc_opts "-g ../tests/custom/common/syscalls.c ../tests/custom/common/crt.S -I../tests/custom/env -I../tests/custom/common -T ../tests/custom/common/test.ld -misa-spec=2.2"
 make -C ../../core-v-cores/cva6 clean
 make clean_all
 python3 cva6.py --testlist=../tests/testlist_riscv-compliance-cv32a60x.yaml --test rv32i-I-ADD-01 --iss_yaml cva6.yaml --target cv32a60x --iss=$DV_SIMULATORS $DV_OPTS
