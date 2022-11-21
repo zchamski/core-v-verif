3dd231b0b76eeb714d1430c2f3013f947d5e883c
Zbigniew Chamski <zbigniew.chamski@thalesgroup.com>
2022-11-21 16:17:49 +0100

    [VPTOOL] Improve Python quality metrics in vp_pack.py.  Reformat w/black.
    * tools/vptool/vptool/vp_pack.py (all over): Add missing docstrings.
      (normalize_tag): Rework control flow according to Python rules.
      (Item.__init__): Set defaults of numericla fields based on actual default
      values in Yaml config.
      (Item.attrval2str): Rework control flow according to Python rules.
      (Item.__str__): Do not output link-to-coverage or comments if either is
      empty.
      (Prop.del_item): Remove dead code.
      (Prop.get_item_name): Refactor.
      (Prop.insert_item): Reformat docstring.
      (class Ip): Ditto.
      (Ip.__init__): Remove unused attribute.
      (Ip.add_property): Remove unused kwarg.
      (Ip.prep_to_save): Reformat docstring.
    
    Signed-off-by: Zbigniew Chamski <zbigniew.chamski@thalesgroup.com>

--- tools/vptool/vptool/vp_pack.py
+++ tools/vptool/vptool/vp_pack.py
@@ -6,9 +6,16 @@
 # Original Author: Zbigniew Chamski (zbigniew.chamski@thalesgroup.com)
 #############################################################################
 ## Config is Project dependent. It is imported from platform_package
-## PYTHON_PATH env variable could be used
 
-import sys, os
+"""
+Manipulate and render basic objects composing the Verification Plan:
+- features
+- sub-features
+- verification items
+"""
+
+import sys
+import os
 from datetime import datetime
 import re
 
@@ -20,29 +27,37 @@ if "PLATFORM_TOP_DIR" in os.environ:
         import vp_config
 
         vp_config.io_fmt_gitrev = "$Id$"
-    except Exception as e:
-        print("ERROR: Please define path to vp_config package (got %s!)" % str(e))
+    except ImportError as e:
+        print("ERROR: Please define path to vp_config package (got " + str(e) + "!)")
         sys.exit(1)
 else:
     print("ERROR: Environment variable 'PLATFORM_TOP_DIR' not set, cannot continue!")
     sys.exit(1)
 
-# Remove non-ASCII characters from a string.
-def remove_non_ascii(s):
-    return "".join([x for x in s if ord(x) < 128])
+
+def remove_non_ascii(text):
+    """Remove non-ASCII characters from a string."""
+    return "".join([x for x in text if ord(x) < 128])
 
 
-# Normalize a heritage VP_IPnnn_Pnnn_Innn tag
-# to VP_<PROJECT_NAME>_Fnnn_Snnn_Innn form.
-def normalize_tag(l):
+def normalize_tag(tag):
+    """
+    Normalize a heritage VP_IPnnn_Pnnn_Innn tag
+    to VP_<PROJECT_IDENT>_Fnnn_Snnn_Innn form.
+    """
     pattern_oldstyle = re.compile(r"VP_IP([0-9]+)_P([0-9]+)_I([0-9]+)$")
-    match = pattern_oldstyle.match(l)
-    if match and match.group() == l:
+    match = pattern_oldstyle.match(tag)
+    if match and match.group() == tag:
         # Full match
         return "VP_" + vp_config.PROJECT_IDENT + "_F%s_S%s_I%s" % match.groups()
-    else:
-        # Partial match or no match at all: return unmodified label.
-        return l
+
+    # Partial match or no match at all: return unmodified label.
+    return tag
+
+
+def yaml_default(field):
+    """Return default value associated with GUI field FIELD in Yaml config."""
+    return vp_config.yaml_config["gui"][field]["default"]["value"]
 
 
 #####################################
@@ -74,11 +89,10 @@ class Item:
         self.verif_goals = ""
         # Location of coverage data
         self.coverage_loc = ""
-        # FIXME: Propagate default value from YAML config.
-        self.pfc = -1  # none selected, must choose
-        self.test_type = -1  # none selected, must choose
-        self.cov_method = -1  # none selected, must choose
-        self.cores = -1  # applicable to all cores
+        self.pfc = yaml_default("pfc")  # none selected, must choose
+        self.test_type = yaml_default("test_type")  # none selected, must choose
+        self.cov_method = yaml_default("cov_method")  # none selected, must choose
+        self.cores = yaml_default("cores")  # applicable to all cores
         self.comments = ""
         self.status = ""
         self.simu_target_list = []
@@ -89,6 +103,7 @@ class Item:
         self.rfu_dict["lock_status"] = 0
 
     def attrval2str(self, attr):
+        """Convert numerical attribute value to human-readable form."""
         if attr == "cores" and "cores" in vp_config.yaml_config:
             # 'cores' are at toplevel of the Yaml config and the attr value is a bitmap.
             # Select entries corresponding to each bit that is set
@@ -100,9 +115,8 @@ class Item:
             ]
             if len(matches) == 0:
                 return "None applicable"
-            else:
-                return ", ".join(matches)
-        elif "values" in vp_config.yaml_config["gui"][attr]:
+            return ", ".join(matches)
+        if "values" in vp_config.yaml_config["gui"][attr]:
             # This attribute takes predefined values.
             # A single value is allowed.
             if (
@@ -110,22 +124,21 @@ class Item:
                 == vp_config.yaml_config["gui"][attr]["default"]["value"]
             ):
                 return "NDY (Not Defined Yet)"
-            else:
-                matches = [
-                    x["label"]
-                    for x in vp_config.yaml_config["gui"][attr]["values"]
-                    if x["value"] == getattr(self, attr)
-                ]
-                if len(matches) == 0:
-                    return "<UNKNOWN>"
-                else:
-                    return matches[0]
-        else:
-            return "N/A (unsupported field '%s')" % attr
+            matches = [
+                x["label"]
+                for x in vp_config.yaml_config["gui"][attr]["values"]
+                if x["value"] == getattr(self, attr)
+            ]
+            if len(matches) == 0:
+                return "<UNKNOWN>"
+            return matches[0]
+        return "N/A (unsupported field '%s')" % attr
 
     def preserve_linebrs(self, text, indent="  "):
-        """Preserve line breaks in the text by inserting two spaces before
-        each newline.  Ensure first line of 'text' starts on a new line."""
+        """
+        Preserve line breaks in the text by inserting two spaces before
+        each newline.  Ensure first line of 'text' starts on a new line.
+        """
         md_linebreak = "  \n" + indent
         return md_linebreak + md_linebreak.join(text.split("\n"))
 
@@ -148,17 +161,23 @@ class Item:
         return0 += format(
             "* **Unique verification tag:** %s\n" % normalize_tag(self.tag)
         )
-        return0 += format("* **Link to Coverage:** %s\n" % self.coverage_loc)
-        return0 += format(
-            "* **Comments**\n%s\n"
-            % self.preserve_linebrs(self.comments if self.comments else "*(none)*\n")
-        )
+        if self.coverage_loc:
+            return0 += format("* **Link to Coverage:** %s\n" % self.coverage_loc)
+        if self.comments:
+            return0 += format(
+                "* **Comments**\n%s\n" % self.preserve_linebrs(self.comments)
+            )
         return return0
 
     def __del__(self):
         self.__class__.count -= 1
 
     def invert_lock(self):
+        """
+        Invert lock status of the current item.
+        If the lock is being set, store current timestamp and user ID inside
+        the lock status entry of the state dict.
+        """
         if self.is_locked():
             self.rfu_dict["lock_status"] = 0
         else:
@@ -167,15 +186,22 @@ class Item:
             )
 
     def unlock(self):
+        """Release the lock on the current item."""
         self.rfu_dict["lock_status"] = 0
 
     def lock(self):
+        """
+        Set the lock on the current item.  Store current timestamp and user ID inside
+        the lock status entry of the state dict.
+        """
         self.rfu_dict["lock_status"] = " ".join((str(datetime.now()), os.getlogin()))
 
     def is_locked(self):
+        """Return True if the current item is locked."""
         return bool(self.rfu_dict["lock_status"])
 
     def get_lock_status(self):
+        """Get the details of lock status of current item."""
         return str(self.rfu_dict["lock_status"])
 
     def prep_to_save(self):
@@ -216,6 +242,7 @@ class Prop:
         return format("### Sub-feature: %s\n\n" % (self.name))
 
     def prop_clone(self):
+        """Clone an existing sub-feature."""
         new_prop = Prop()
         new_prop.item_count = self.item_count
         new_prop.name = self.name
@@ -228,7 +255,8 @@ class Prop:
         new_prop.rfu_dict = self.rfu_dict.copy()
         return new_prop
 
-    def add_item(self, tag, description="", purpose=""):  # adds an item to Prop
+    def add_item(self, tag, description="", purpose=""):
+        """Add new item to current sub-feature."""
         new_item = Item(
             str(self.item_count).zfill(3),
             tag=tag + "_I" + str(self.item_count).zfill(3),
@@ -240,23 +268,22 @@ class Prop:
         # Return a ref to the newly created item.
         return new_item
 
-    def del_item(self, index):  # remove an item from Prop
-        if (
-            index == max(self.item_list.keys()) and False
-        ):  # Spare numbering option disabled by False statement.
-            self.item_count -= 1  # if the element removed is the last one, one can decrement item_count to spare numbering
+    def del_item(self, index):
+        """Remove an item at INDEX from current sub-feature."""
         del self.item_list[index]
 
     def get_item_name(self):
-        item_list_name = []
+        """Get list of names of all items in current sub-feature."""
+        item_name_list = []
         for item in self.item_list:
-            item_list_name.append(item.name)
-        return item_list_name
+            item_name_list.append(item.name)
+        return item_name_list
 
     def prep_to_save(self):
         """
         Trick used to ensure pickle output file stability
-        Pickle doesn't provide reproductible output for dict. When saved, they are converted to list
+        Pickle doesn't provide reproductible output for dicts.
+        When saved, they are converted to lists.
         """
         # Sanitize items of this Prop.
         for item in self.item_list.values():
@@ -274,9 +301,12 @@ class Prop:
         self.rfu_list = []
 
     def insert_item(self, item_name):
-        """This is intended to be used in specific cases as it
-        can changes every item numbering; Should not be used ater item is implemented in simulations
-        It insert last item in self.item_list at insert index, and update item tag and name accordingly
+        """
+        This is intended to be used in specific cases as it
+        can change every item numbering;
+        Should not be used after item is implemented in simulations
+        It inserts last item in self.item_list at insert index, and updates
+        item tag and name accordingly
         """
         insert_index = int(item_name) + 1
         updated_dict = {}
@@ -301,18 +331,20 @@ class Prop:
         self.item_list = updated_dict
 
     def unlock_items(self):
+        """Unlock all items in current sub-feature."""
         for item in list(self.item_list.values()):
             item.unlock()
 
     def lock_items(self):
+        """Lock all items in current sub-feature."""
         for item in list(self.item_list.values()):
             item.lock()
 
 
 class Ip:
     """
-    An IP defines a bloc instantiated at chip top level, or more generally, a design specification chapter
-    needing to be covered by a verification plan.
+    An IP defines a bloc instantiated at chip top level, or more generally,
+    a design specification chapter that needs to be covered by a verification plan.
     It contains a collection of Prop.
     """
 
@@ -331,13 +363,13 @@ class Ip:
         # rfu for future dev
         self.rfu_dict = {}
         self.rfu_list = []
-        self.rfu_list_0 = []
         self.rfu_list_1 = []
 
     def __str__(self):
         return format("## Feature: %s\n\n" % (self.name))
 
-    def add_property(self, name, tag="", custom_num=""):  # adds an Prop instance to Ip
+    def add_property(self, name, custom_num=""):
+        """Add a new sub-feature to current feature."""
         if name in list(self.prop_list.keys()):
             print("Property already exists")
             feedback = 0
@@ -358,7 +390,8 @@ class Ip:
             self.prop_count += 1
         return (feedback, prop_name)
 
-    def del_property(self, name):  # remove a Prop instance from Ip
+    def del_property(self, name):  #
+        """Remove a sub-feature from current feature."""
         if (
             self.prop_count == int(self.prop_list[name].tag[-3:]) + 1
         ):  # with custom numbering max elt is not always the last one created
@@ -366,6 +399,7 @@ class Ip:
         del self.prop_list[str(name)]
 
     def clear(self):
+        """Reset sub-feature count of current feature."""
         self.__class__._ip_count = 0
 
     def unlock_properties(self):
@@ -391,7 +425,8 @@ class Ip:
     def prep_to_save(self):
         """
         Trick used to ensure pickle output file stability
-        Pickle doesn't provide reproducible output for dicts. When saved, they are converted to lists.
+        Pickle doesn't provide reproducible output for dicts. When saved, they are converted
+        to lists.
         """
         self.rfu_list = sorted(list(self.prop_list.items()), key=lambda key: key[0])
         self.prop_list = {}
@@ -406,6 +441,7 @@ class Ip:
         self.rfu_list = []
 
     def create_ip_tag_dict(self):
+        """Create a tag->item dictionary of the current feature."""
         ip_tag_dict = {}
         for prop in list(self.prop_list.values()):
             for item in list(prop.item_list.values()):
